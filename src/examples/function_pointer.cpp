b34159ccf9f7d9d8e1aa32d9e81aa596d0744011
yujie <yjwen.ty@gmail.com>
2013-10-16 22:16:29 +0800

    C++的函数指针

--- src/examples/function_pointer.cpp
+++ src/examples/function_pointer.cpp
@@ -1,40 +1,16 @@
 #include <string>
 #include <iostream>
 
-using namespace std;
-typedef string (*str_func_type)(string const &, string const &);
-string apply_on_pair(str_func_type func,
-                     string const &a, string const &b)
+template<typename T>
+T chain_apply(T (*func)(T const&), T const &v)
 {
-  return func(a, b);
+  return func(func(v));
 }
 
-// 返回函数指针类型的元函数
-template<typename T>
-struct func_type
-{
-  //类型result是元函数的返回类型
-  typedef T (*result)(T const&, T const&);
-};
-  
-template<typename T>
-T apply_on_pair(typename func_type<T>::result func,
-                // ↑利用元函数得到函数指针类型↑
-                T const& a, T const& b)
-{
-  return func(a, b);
-}
-
-template<typename T>
-T print_pair(T const &a, T const &b)
-{
-  std::cout << a << ", " << b << std::endl;
-  return b;
-}
+int square(int const &v) {return v * v;}
 
 int main()
 {
-  apply_on_pair(print_pair, string("abc"), string("def"));
-  apply_on_pair(print_pair, 1, 2);
+  std::cout << chain_apply(square, 2) << std::endl;
   return 0;
 }
