777b08b240e938102c9b82bf9b1c299f4c50a93c
yjwen <yjwen.ty@gmail.com>
2013-10-23 22:36:18 +0800

    利用C++11的元函数及lambda函数实现闭合的算法复用机制

--- src/examples/function_object.cpp
+++ src/examples/function_object.cpp
@@ -3,26 +3,7 @@
 #include <cmath>
 #include <sstream>
 
-// // 将单参数函数封装成函数对象的封装类模板
-// template<typename RetT, typename T>
-// struct unary_func
-// {
-//   typedef T param_type;         // 参数类型
-//   typedef RetT return_type;     // 返回值类型
-//   typedef return_type (*func_type)(param_type); // 函数指针类型
-//   func_type func_ptr;                           // 所封装的函数指针
-//   unary_func(func_type func_ptr) : func_ptr(func_ptr) {}
-//   // 重载的括号操作符，直接调用所封装的函数。
-//   return_type operator() (param_type v) {return func_ptr(v);}
-// };
-
-// // 生成封装类的助手函数模板
-// template<typename RetT, typename T>
-// unary_func<RetT, T> unary(RetT (*func_ptr)(T))
-// {
-//   return unary_func<RetT, T>(func_ptr);
-// }
-
+// 提取函数对象返回值的元函数
 template<typename T>
 struct function_traits
 {
@@ -35,13 +16,14 @@ struct function_traits<R (C::*)(A...)>
 {
   typedef R return_type;
 };
+// 元函数到此结束
 
 // 连锁调用两个单参数函数的函数对象
 template<typename Func0,        // Func0与Func1为所调用的函数对象类型。
          typename Func1>
 struct chain_func
 {
-  // 按照约定提取出函数对象的参数类型和返回值类型
+  // 利用元函数提取出Func0的返回值类型
   typedef typename function_traits<Func0>::return_type return_type;
   Func0 func0;                  // 所复用的函数对象
   Func1 func1;
@@ -58,17 +40,21 @@ chain_func<Func0, Func1> chain(Func0 func_ptr0, Func1 func_ptr1)
   return chain_func<Func0, Func1>(func_ptr0, func_ptr1);
 }
 
-// bool invert(bool v) {return !v;}
-// using namespace std;
-// string bool_to_string(bool v) {return string(v ? "true" : "false");}
-
 void test_chain()
 {
-  auto is_odd = [](int v) {return v % 2 == 1;};
-  auto invert = [](bool v) {return !v;};
-  auto is_even = chain(invert, is_odd);
+  // 利用C++11中的lambda函数生成函数对象
+  auto is_odd = [](int v) mutable {return v % 2 == 1;};
+  auto invert = [](bool v) mutable {return !v;};
+  auto bool_to_string = [](bool v) mutable {
+    return std::string(v ? "true" : "false");
+  };
 
-  std::cout << is_even(3) << std::endl;
+  // 用两次chain生成三个函数对象的连锁调用函数。
+  auto is_even = chain(invert, is_odd);
+  auto is_even_str = chain(bool_to_string, is_even);
+
+  // 将打印字符串"true"
+  std::cout << is_even_str(4) << std::endl;
 
 }
 int main()
